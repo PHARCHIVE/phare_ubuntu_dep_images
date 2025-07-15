81e5ac2279996dd8ebef7efe0fc87062b2609dd8
PhilipDeegan <philip.deegan@gmail.com>
2025-07-15 13:22:13 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -ex
 
 apt-get update
 apt-get install -y git make cmake libopenmpi-dev libhdf5-openmpi-dev g++ ninja-build \
-                   ccache python3-dev python3-pip gfortran python3-venv
+                   ccache python3-dev python3-pip gfortran python3-venv gfortran
 
 git clone https://github.com/PHAREHUB/PHARE -b master --depth 1 --recursive --shallow-submodules phare
 
@@ -21,11 +21,11 @@ python3 -m venv .venv
 
 )
 
-(
+( 
     cd phare/subprojects/samrai
     mkdir build
     cd build
-    cmake .. -DENABLE_SAMRAI_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
+    cmake .. -DBUILD_SHARED_LIBS=ON -DENABLE_SAMRAI_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
     make -j2
     make install
     cd ../..
